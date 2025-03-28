cd0cb1a239f9dbd235b5dafe807d0b8e2c2eccb7
Marcos Vinícius Silva <marcoscv71@gmail.com>
2025-03-28 10:59:25 -0300

    Create get-user-profile case use

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,12 @@
 import { prisma } from "@/lib/prisma";
-import { Prisma } from "@prisma/client";
+import { Prisma, type User } from "@prisma/client";
 import type { UsersRepository } from "../users-repository";
 
 export class PrismaUsersRepository implements UsersRepository {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  findById(id: string): Promise<User | null> {
+    throw new Error("Method not implemented.");
+  }
   async findByEmail(email: string) {
     // Verifica se ja existe um usuario com email cadastrado
     const user = await prisma.user.findUnique({
